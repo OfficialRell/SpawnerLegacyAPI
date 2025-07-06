0e0f867abbe358b20bf82489cc55967b9b1f8ebb
Rellox <unconfigured@null.spigotmc.org>
2025-07-06 15:16:37 +0300

    Added ticker class

---
+++ src/main/java/mc/rellox/spawnerlegacyapi/utility/ITicker.java
@@ -0,0 +1,47 @@
+package mc.rellox.spawnerlegacyapi.utility;
+
+public interface ITicker {
+	
+	static ITicker of(int value) {
+		return of(value, false);
+	}
+	
+	static ITicker of(int value, boolean offset) {
+		return new Ticker(value, offset);
+	}
+	
+	/**
+	 * Ticks this ticker
+	 */
+	
+	void tick();
+	
+	/**
+	 * @return Is tick value {@code 0}
+	 */
+	
+	boolean valid();
+	
+	class Ticker implements ITicker {
+		
+		private final int value;
+		private int ticks = 1;
+		
+		public Ticker(int value, boolean offset) {
+			this.value = value;
+			if(offset == true) ticks = Calculate.random(value);
+		}
+
+		@Override
+		public void tick() {
+			if(--ticks < 0) ticks = value - 1;
+		}
+
+		@Override
+		public boolean valid() {
+			return ticks == 0;
+		}
+		
+	}
+
+}
