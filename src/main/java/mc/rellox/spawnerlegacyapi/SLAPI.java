f50cae50a6f00cf622cc4f78500c542c8a3df6e9
Rellox <unconfigured@null.spigotmc.org>
2025-07-06 15:17:50 +0300

    Added static getter methods

--- src/main/java/mc/rellox/spawnerlegacyapi/SLAPI.java
+++ src/main/java/mc/rellox/spawnerlegacyapi/SLAPI.java
@@ -3,6 +3,16 @@ package mc.rellox.spawnerlegacyapi;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
+import mc.rellox.spawnerlegacyapi.manager.ICacheManager;
+import mc.rellox.spawnerlegacyapi.manager.IGeneratorManager;
+import mc.rellox.spawnerlegacyapi.manager.IHologramManager;
+import mc.rellox.spawnerlegacyapi.manager.ILangaugeManager;
+import mc.rellox.spawnerlegacyapi.manager.ILayoutManager;
+import mc.rellox.spawnerlegacyapi.manager.ISettingsManager;
+import mc.rellox.spawnerlegacyapi.manager.ISpawnerManager;
+import mc.rellox.spawnerlegacyapi.manager.IValueManager;
+import mc.rellox.spawnerlegacyapi.manager.IViewManager;
+
 /**
  * Spawner Legacy API class
  */
@@ -19,6 +29,74 @@ public final class SLAPI {
 	
 	public static ISpawnerLegacy get() {
 		return INSTANCE;
+	}
+	
+	public static IGeneratorManager generators() {
+		return INSTANCE.generators();
+	}
+	
+	/**
+	 * @return Hologram manager
+	 */
+	
+	public static IHologramManager holograms() {
+		return INSTANCE.holograms();
+	}
+	
+	/**
+	 * @return Setting value manager
+	 */
+	
+	public static IValueManager values() {
+		return INSTANCE.values();
+	}
+	
+	/**
+	 * @return Cache type manager
+	 */
+	
+	public static ICacheManager cache() {
+		return INSTANCE.cache();
+	}
+	
+	/**
+	 * @return Spawner value and item manager
+	 */
+	
+	public static ISpawnerManager spawners() {
+		return INSTANCE.spawners();
+	}
+	
+	/**
+	 * @return Inventory view manager
+	 */
+	
+	public static IViewManager views() {
+		return INSTANCE.views();
+	}
+	
+	/**
+	 * @return Language/translation manager
+	 */
+	
+	public static ILangaugeManager language() {
+		return INSTANCE.language();
+	}
+	
+	/**
+	 * @return Layout manager
+	 */
+	
+	public static ILayoutManager layouts() {
+		return INSTANCE.layouts();
+	}
+	
+	/**
+	 * @return Settings manager
+	 */
+	
+	public static ISettingsManager settings() {
+		return INSTANCE.settings();
 	}
 	
 	public interface ISpawnerLegacyPlugin extends Plugin {
