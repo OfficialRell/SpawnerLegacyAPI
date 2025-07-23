8e5db8138eab596d3efb317599830212821802d7
Rellox <unconfigured@null.spigotmc.org>
2025-07-23 15:09:17 +0300

    Added comments

--- src/main/java/mc/rellox/spawnerlegacyapi/modifier/IModifierRequirements.java
+++ src/main/java/mc/rellox/spawnerlegacyapi/modifier/IModifierRequirements.java
@@ -6,11 +6,31 @@ import mc.rellox.spawnerlegacyapi.spawner.type.SpawnerType;
 
 public interface IModifierRequirements {
 	
+	/**
+	 * @return Required spawner level to work
+	 */
+	
 	int level();
+	
+	/**
+	 * Only will work on spawner who are in this list,
+	 * or is not if {@link #invert} is true.
+	 * 
+	 * @return Spawner type set
+	 */
 	
 	Set<SpawnerType> types();
 	
+	/**
+	 * @return Should invert the type list check
+	 */
+	
 	boolean invert();
+	
+	/**
+	 * @param type - type
+	 * @return If this type is allowed
+	 */
 	
 	default boolean allow(SpawnerType type) {
 		return types().contains(type) == invert();
