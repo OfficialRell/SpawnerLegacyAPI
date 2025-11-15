4728fae7e270bf884428fdd41c4437a0f4d42dee
Rellox <unconfigured@null.spigotmc.org>
2025-11-15 23:20:00 +0200

    Added spawner types GUI layout and comments

--- src/main/java/mc/rellox/spawnerlegacyapi/manager/ILayoutManager.java
+++ src/main/java/mc/rellox/spawnerlegacyapi/manager/ILayoutManager.java
@@ -7,13 +7,47 @@ import mc.rellox.spawnerlegacyapi.view.layout.order.IOrder;
 
 public interface ILayoutManager {
 	
+	/**
+	 * Gets the layout item that is saved in the global item list.
+	 * 
+	 * @param key - layout item key
+	 * @return Layout item
+	 */
+	
 	IViewItem item(String key);
+	
+	/**
+	 * Gets the layout for the upgrade GUI of the given spawner type.
+	 * 
+	 * @param type - type
+	 * @return Layout for upgrade GUI
+	 */
 	
 	ILayout upgrades(SpawnerType type);
 	
+	/**
+	 * @return Layout for purchase shop GUI
+	 */
+	
 	ILayout purchase();
 	
+	/**
+	 * @return Layout for sell shop GUI
+	 */
+	
 	ILayout sell();
+	
+	/**
+	 * @return Layout for spawner types GUI
+	 */
+	
+	ILayout types();
+	
+	/**
+	 * 
+	 * @param key - order key
+	 * @return Item orderer, never {@code null}
+	 */
 	
 	IOrder order(String key);
 
