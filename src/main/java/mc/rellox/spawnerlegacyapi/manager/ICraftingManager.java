8437fa7b50ae0d6a4709208ac5a266b32533270f
Rellox <unconfigured@null.spigotmc.org>
2026-02-16 21:09:02 +0200

    Added more methods for spawner recipes and results

--- src/main/java/mc/rellox/spawnerlegacyapi/manager/ICraftingManager.java
+++ src/main/java/mc/rellox/spawnerlegacyapi/manager/ICraftingManager.java
@@ -1,18 +1,28 @@
 package mc.rellox.spawnerlegacyapi.manager;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
 import mc.rellox.spawnerlegacyapi.crafting.ISpawnerCraft;
+import mc.rellox.spawnerlegacyapi.crafting.ISpawnerRecipe;
 
 public interface ICraftingManager {
+	
+	/**
+	 * Gets all registered recipes.
+	 * 
+	 * @return List of recipes
+	 */
+	
+	List<ISpawnerRecipe> recipes();
 	
 	/**
 	 * Gets the active craft for a player by UUID.
 	 * 
 	 * @param id - player id
-	 * @return Active craft, or {@code null} if none exists
+	 * @return Craft, or {@code null} if none exists
 	 */
 	
 	ISpawnerCraft craft(UUID id);
@@ -21,7 +31,7 @@ public interface ICraftingManager {
 	 * Gets the active craft for a player.
 	 * 
 	 * @param player - player
-	 * @return Active craft, or {@code null} if none exists
+	 * @return Craft, or {@code null} if none exists
 	 */
 	
 	ISpawnerCraft craft(Player player);
@@ -30,7 +40,7 @@ public interface ICraftingManager {
 	 * Checks if the player is currently crafting.
 	 * 
 	 * @param id - player id
-	 * @return {@code true} if crafting, {@code false} otherwise
+	 * @return {@code true} if crafting
 	 */
 	
 	boolean crafting(UUID id);
@@ -39,7 +49,7 @@ public interface ICraftingManager {
 	 * Checks if the player is currently crafting.
 	 * 
 	 * @param player - player
-	 * @return {@code true} if crafting, {@code false} otherwise
+	 * @return {@code true} if crafting
 	 */
 	
 	boolean crafting(Player player);
@@ -53,11 +63,55 @@ public interface ICraftingManager {
 	void start(ISpawnerCraft craft);
 	
 	/**
-	 * Removes the craft.
+	 * Cancels and removes the craft, if any.
 	 * 
 	 * @param id - player id
 	 */
 	
 	void remove(UUID id);
+	
+	/**
+	 * Cancels and removes the craft, if any.
+	 * 
+	 * @param player - player
+	 */
+	
+	void remove(Player player);
+	
+	/**
+	 * Gets the crafting result.
+	 * 
+	 * @param id - player id
+	 * @return Crafting result, or {@code null} if none exists
+	 */
+	
+	ISpawnerRecipe result(UUID id);
+	
+	/**
+	 * Gets the crafting result.
+	 * 
+	 * @param player - player
+	 * @return Crafting result, or {@code null} if none exists
+	 */
+	
+	ISpawnerRecipe result(Player player);
+	
+	/**
+	 * Takes the crafting result, removing it.
+	 * 
+	 * @param id - player id
+	 * @return Crafting result, or {@code null} if none exists
+	 */
+	
+	ISpawnerRecipe take(UUID id);
+	
+	/**
+	 * Takes the crafting result, removing it.
+	 * 
+	 * @param player - player
+	 * @return Crafting result, or {@code null} if none exists
+	 */
+	
+	ISpawnerRecipe take(Player player);
 	
 }
